42518c6dadb08b660b9d911193697f08ed13fbd8
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-04-16 21:23:25 +0800

    wip

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const PORT  = process.env.PORT || 3001
+const PORT  = process.env.PORT || 3000
 const server = app.listen(PORT,()=>{
     console.log(`Server is running ${PORT}`);
 });
